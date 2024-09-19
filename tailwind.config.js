10d7887efaf29ea8305b12a2a918d0fe65997b94
ethanmcquhae <ethan.mcq01@gmail.com>
2024-09-19 12:05:08 -0600

    lots of homepage changes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], 
+  theme: {
+    extend: {
+      colors:
+      {
+        mainPurple: "#571f8a"
+      }
+    },
+  },
+  plugins: [],
+}
